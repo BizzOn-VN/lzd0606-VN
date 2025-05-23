277bebd6c0a0abb8120d682ecff5112428998496
chikien111 <38467294+chikien111@users.noreply.github.com>
2025-05-23 14:47:43 +0700

    update page

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* HELPER: Checks Whether an Element Exists
+----------------------------------------------------------------------------------------------------*/
+(function( $ ) {
+
+  $.fn.extend({
+    exists: function() {
+      if ( this.length > 0 ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+
+})( jQuery );
+
+
+
+jQuery(document).on("ready",function () {
+	
+});
+
+
+
+
+
+
+
+
+
+ $('.md-boxes .md-col').height(h3height); 
+ var h3height = 0;
+     $('.md-boxes .md-col').each(function() {
+         if(h3height < $(this).height()){
+           h3height = $(this).height();
+         };
+     });
+ $('.md-boxes .md-col').height(h3height); 
+ var onresize = function() {
+    $('.md-boxes .md-col').css("height","auto");
+            var h3height = 0;
+            $('.md-boxes .md-col').each(function() {
+                if(h3height < $(this).height()){
+                  h3height = $(this).height();
+                };
+            });
+            $('.md-boxes .md-col').height(h3height);
+    };
+ window.addEventListener("resize", onresize);
+
+    $("#md-popup-2").fancybox().trigger('click');
+    // $("#md-popup-3").fancybox().trigger('click');
+
+
+      
+
